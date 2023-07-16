5dd54e4802b88929b572fd69d8f5c5a0fa85ac24
root <root@ip-172-31-38-145.ap-south-1.compute.internal>
2023-07-16 11:50:49 +0000

    security_groups added

--- EC2terraform.tf
+++ EC2terraform.tf
@@ -1,15 +1,30 @@
 provider "aws" {
-
  region = "ap-south-1"
- access_key = "AKIAU6B77CZ5DV346Q2J"
- secret_key = "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"
+ access_key = "AKIAU6B77CZ5HDKQ5XAR"
+ secret_key = "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"
 }
-
 resource "aws_instance" "TOMCAT_SERVER" {
- 
  ami = "ami-006935d9a6773e4ec"
  instance_type = "t2.micro"
- key_name = "1st_keypair"
+ key_name =  "1st_keypair"
+ monitoring = "true"
+ security_groups = ["1st_Security_grp"]
+ vpc_security_group_ids = ["sg-0e27cba5b95fe7546"]
+ tags = {
+    Name = "TOMCAT_SERVER"
 }
+}
+resource "aws_security_group" "TERRAFORM_SEC_GP" {
+  name = "terraform_Security_group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
+
 
 
